83a8739586c4e9e968a8dd24d69b1d54fda7b243
felix0746 <johnsonyao466@gmail.com>
2025-08-26 15:44:35 +0800

    feat: Add Google Search Console verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout({
     <html lang="zh-Hant">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        <meta name="google-site-verification" content="wry9ix6DEHAyH4l-rcrpVhbVGC8mXgO_hFMEOj412BI" />
       </head>
       <body className={`${notoSerif.variable} ${notoSans.variable}`}>
         <LanguageProvider>
